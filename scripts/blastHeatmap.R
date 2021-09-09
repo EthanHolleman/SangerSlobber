ab87d2995ec6ea7352f636b6df2054eac511d19a
Your Name <etholleman@ucdavis.edu>
2021-09-08 20:17:22 -0700

    start reader and plot functions

--- scripts/blastHeatmap.R
+++ scripts/blastHeatmap.R
@@ -2,7 +2,24 @@
 
 read.outfmt.six <- function(file.path){
 
+    file.path <- as.character(file.path)
+    blast.df <- as.data.frame(read.table(file.path, sep='\t', header=FALSE))
+    fmt.six.headers <- c(
+        "qseqid","sseqid","pident","length","mismatch","gapopen",
+        "qstart","qend","sstart","send","evalue","bitscore"
+        )
+    colnames(blast.df) <- fmt.six.headers
+
+    return(blast.df)
     
+}
+
+
+make.heatmap <- function(blast.df, fill){
+
+
+    ggplot(blast.df, aes_string(x="qseqid", y="sseqid", fill=fill)) +
+            geom_tile(color='black') + theme_pubr()
 
 
 }
